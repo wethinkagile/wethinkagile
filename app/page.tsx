81e97d802aa470a54e02a4086b9f86350db3cc1b
Stephan Kristyn <hallo@shiftcloud.de>
2024-01-11 01:23:43 +0100

    'fixing of bugs'

--- app/page.tsx
+++ app/page.tsx
@@ -41,14 +41,13 @@ export default async function Home() {
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
-        >
-          Stephan Kristyn - IT-Solutions Architect
+        >InfoSec Regulatory DevOps 2.0
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
         >
-          Beauftragen Sie einen erfahrenen "Reflective practitioner" aus München um Ihr komplexes Projekt zu stemmen.
+          Beauftragen Sie einen erfahrenen &quot;Reflective practitioner&quot; aus München um Ihr komplexes Projekt zu stemmen.
         </p>
         <div
           className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
--- components/home/demo-modal.tsx
+++ components/home/demo-modal.tsx
@@ -28,7 +28,7 @@ const DemoModal = ({
               height={20}
             />
           </a>
-          <h3 className="font-display text-2xl font-bold">München, den 11. Januar '24</h3>
+          <h3 className="font-display text-2xl font-bold">München, den 11. Jan. 2024</h3>
           <p className="text-sm text-gray-500">
             Ich bin ein Freelancer, der als Scrum Master, Product Owner oder Solutions Architekt im gesamttechnischen Kontext
             Ihres Projektes tätig werden kann.
