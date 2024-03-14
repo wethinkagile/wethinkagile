92c9077a26f62f70aa32ce5c8aa4088f0cdd3b5c
Stephan Kristyn <hallo@shiftcloud.de>
2024-03-14 17:05:56 +0100

    'updates'

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -31,7 +31,7 @@ export default function NavBar({ session }: { session: Session | null }) {
               className="mr-2 rounded-sm"
             ></Image>
             <p><b>WeThinkAgile</b></p>
-              <small>Beratung und Produktentwicklung</small>
+              &nbsp;&nbsp; - <small>Beratung und Produktentwicklung</small>
           </Link>
           <div>
             {session ? (
