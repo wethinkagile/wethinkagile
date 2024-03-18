9c54c3896b2882a0548a1939be6fb9b7cffaa303
Stephan Kristyn <hallo@shiftcloud.de>
2024-03-19 00:34:07 +0100

    ''

--- app/page.tsx
+++ app/page.tsx
@@ -75,7 +75,7 @@ return (
                     strokeLinejoin="round"
                 />
               </svg>
-              <p>Sofortiger Roll-Out</p>
+              <p>Releasen</p>
             </a>
             <a
                 className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
@@ -85,7 +85,7 @@ return (
             >
               <Github/>
               <p>
-                <span className="hidden sm:inline-block">Auf</span> GitHub merken{" "}
+                <span className="hidden sm:inline-block">Merken</span>{" "}
                 <span className="font-semibold">{nFormatter(stars)}</span>
               </p>
             </a>
