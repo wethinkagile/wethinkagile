b6a167cfaa208934fe72787eca38c78483604409
Stephan Kristyn <hallo@shiftcloud.de>
2024-03-20 06:08:52 +0100

    'updates'

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,7 @@ return (
             </a>
             <a
                 className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-                href="https://github.com/stevek-pro/wethinkagile"
+                href="https://github.com/wethinkagile"
                 target="_blank"
                 rel="noopener noreferrer"
             >
